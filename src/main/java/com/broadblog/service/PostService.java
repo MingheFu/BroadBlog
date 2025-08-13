8c15b2af74966e2aed117c67e6bbdb0933ec4e1b
Minghe FU <minghef@uci.edu>
2025-08-13 17:06:39 +0800

    es account info got

--- src/main/java/com/broadblog/service/PostService.java
+++ src/main/java/com/broadblog/service/PostService.java
@@ -10,11 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.PageImpl;
 
 import com.broadblog.entity.Post;
 import com.broadblog.entity.Tag;
